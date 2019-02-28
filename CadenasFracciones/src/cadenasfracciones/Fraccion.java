509f86508310f5324709506e88ec6723f52ef1f9
EdgarMtz00 <edgar.agustin.martinez@gmail.com>
2019-02-28 08:14:09 -0600

    AÃ±ade clase de fracciones

---
+++ CadenasFracciones/src/cadenasfracciones/Fraccion.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cadenasfracciones;
+
+/**
+ *
+ * @author Agustin
+ */
+public class Fraccion {
+    
+}
