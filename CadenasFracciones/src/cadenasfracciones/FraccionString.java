780048df38d1c3a4eb8a6d8bb3164f834bc10287
EdgarMtz00 <edgar.agustin.martinez@gmail.com>
2019-02-28 08:39:53 -0600

    Agrega clase de strings y fracciones

---
+++ CadenasFracciones/src/cadenasfracciones/FraccionString.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cadenasfracciones;
+
+import java.util.HashMap;
+
+/**
+ *
+ * @author Agustin
+ */
+public class FraccionString {
+    HashMap<String, Integer> enteros;
+    
+    public FraccionString(){
+        enteros.put("uno", 1);
+        enteros.put("un", 1);
+        enteros.put("dos", 2);
+        enteros.put("tres", 3);
+        enteros.put("cuatro", 4);
+        enteros.put("cinco", 5);
+        enteros.put("seis", 6);
+        enteros.put("siete", 7);
+        enteros.put("ocho", 8);
+        enteros.put("nueve", 9);
+        enteros.put("diez", 10);
+        enteros.put("dieci", 10);
+        enteros.put("once", 11);
+        enteros.put("doce", 12);
+        enteros.put("trece", 13);
+        enteros.put("catorce", 14);
+        enteros.put("quince", 15);
+        enteros.put("treinta", 30);
+        enteros.put("treinta y", 30);
+        
+    }
+}
