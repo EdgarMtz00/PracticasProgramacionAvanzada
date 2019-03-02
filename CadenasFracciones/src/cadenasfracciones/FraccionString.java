ca407dd71d624d20fd49d1a29e0345fa291d1dec
EdgarMtz00 <edgar.agustin.martinez@gmail.com>
2019-03-01 22:05:47 -0600

    HashMap con numeros escritos

--- CadenasFracciones/src/cadenasfracciones/FraccionString.java
+++ CadenasFracciones/src/cadenasfracciones/FraccionString.java
@@ -12,28 +12,66 @@ import java.util.HashMap;
  * @author Agustin
  */
 public class FraccionString {
-    HashMap<String, Integer> enteros;
+    HashMap<String, Integer> numeros;
     
     public FraccionString(){
-        enteros.put("uno", 1);
-        enteros.put("un", 1);
-        enteros.put("dos", 2);
-        enteros.put("tres", 3);
-        enteros.put("cuatro", 4);
-        enteros.put("cinco", 5);
-        enteros.put("seis", 6);
-        enteros.put("siete", 7);
-        enteros.put("ocho", 8);
-        enteros.put("nueve", 9);
-        enteros.put("diez", 10);
-        enteros.put("dieci", 10);
-        enteros.put("once", 11);
-        enteros.put("doce", 12);
-        enteros.put("trece", 13);
-        enteros.put("catorce", 14);
-        enteros.put("quince", 15);
-        enteros.put("treinta", 30);
-        enteros.put("treinta y", 30);
+        numeros.put("cero", 0);
+        numeros.put("uno", 1);
+        numeros.put("dos", 2);
+        numeros.put("tres", 3);
+        numeros.put("cuatro", 4);
+        numeros.put("cinco", 5);
+        numeros.put("seis", 6);
+        numeros.put("siete", 7);
+        numeros.put("ocho", 8);
+        numeros.put("nueve", 9);
+        numeros.put("diez", 10);
+        
+        // 11-20
+        numeros.put("once", 11);
+        numeros.put("doce", 12);
+        numeros.put("trece", 13);
+        numeros.put("catorce", 14);
+        numeros.put("quince", 15);
+        numeros.put("dieci", 10);
+        numeros.put("veinte", 20);
+        
+        numeros.put("veinti", 20);
+        
+        // 30-100
+        numeros.put("treinta", 30);
+        numeros.put("cuarenta", 40);
+        numeros.put("cincuenta", 50);
+        numeros.put("sesenta", 60);
+        numeros.put("setenta", 70);
+        numeros.put("ochenta", 80);
+        numeros.put("noventa", 90);
+        
+        numeros.put("quinientos", 500);
+        numeros.put("milesimos", 1000);
+        
+        // multiplicativos
+        numeros.put("cientos", 100);
+        numeros.put("mil", 1000);
+        numeros.put("centesimo", 100);
+        numeros.put("milesimo", 1000);
+        
+        // 100
+        numeros.put("ciento", 100);
+        numeros.put("cien", 100);
+        
+        
+        // denominadores
+        numeros.put("entero", 1);
+        numeros.put("medio", 2);
+        numeros.put("tercio", 3);
+        numeros.put("cuarto", 4);
+        numeros.put("quinto", 5);
+        numeros.put("sexto", 6);
+        numeros.put("septimo", 7);
+        numeros.put("octavo", 8);
+        numeros.put("noveno", 9);
+        numeros.put("decimo", 10);
         
     }
 }
