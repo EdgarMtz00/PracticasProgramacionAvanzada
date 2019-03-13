590a51764de1e8226d66b0ad2bcc8125170f6c33
EdgarMtz00 <edgar.agustin.martinez@gmail.com>
2019-03-12 19:41:15 -0600

    operaciones de fracciones

--- CadenasFracciones/src/cadenasfracciones/Fraccion.java
+++ CadenasFracciones/src/cadenasfracciones/Fraccion.java
@@ -10,5 +10,43 @@ package cadenasfracciones;
  * @author Agustin
  */
 public class Fraccion {
+    int num, den;
     
+    public Fraccion(){
+        num = 1;
+        den = 1;
+    }
+    
+    public Fraccion(int num, int den){
+        if(den != 0){
+            this.num = num;
+            this.den = den;
+        }else{
+            System.out.println("");
+        }
+    }
+    
+    public Fraccion suma(Fraccion f1){
+        int rNum = num * f1.den + f1.num * den;
+        int rDen = den * f1.den;
+        return new Fraccion(rNum, rDen);
+    }
+    
+    public Fraccion resta(Fraccion f1){
+        int rNum = num * f1.den - f1.num * den;
+        int rDen = den * f1.den;
+        return new Fraccion(rNum, rDen);
+    }
+    
+    public Fraccion mult(Fraccion f1){
+        int rNum = num * f1.num;
+        int rDen = den * f1.den;
+        return new Fraccion(rNum, rDen);
+    }
+    
+    public Fraccion div(Fraccion f1){
+        int rNum = num * f1.den;
+        int rDen = den * f1.num;
+        return new Fraccion(rNum, rDen);
+    }
 }
