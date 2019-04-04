991911e8c427d563d80d4c05ac3339216cc5396c
EdgarMtz00 <edgar.agustin.martinez@gmail.com>
2019-04-03 20:46:49 -0600

    importacion a IntelliJ

--- CocoBotones/src/cocobotones/Ej2.java
+++ CocoBotones/src/cocobotones/Ej2.java
@@ -14,7 +14,7 @@ import javax.swing.JFrame;
  * @author Agustin
  */
 public class Ej2 extends JFrame{
-    JButton[] cosas = new JButton[10];
+    JButton[] cosas = new JButton[12];
     GroupLayout orden = new GroupLayout(this.getContentPane());
     public Ej2(){
         for(JButton cosa : cosas){
@@ -29,12 +29,25 @@ public class Ej2 extends JFrame{
            orden.createSequentialGroup()
                 .addGroup(orden.createParallelGroup()
                     .addGroup(orden.createSequentialGroup()
-                            .addComponent(cosas[1], 300, 300, 300)
-                            .addComponent(cosas[2], 150, 150, 150)
+                        .addComponent(cosas[1], 300, 300, 300)
+                        .addGroup(orden.createParallelGroup()
+                            .addComponent(cosas[2])
+                            .addComponent(cosas[5])
+                            .addGroup(orden.createSequentialGroup()
+                                .addComponent(cosas[7])
+                                .addComponent(cosas[8])
+                            )
+                        )
                     )
+                    .addComponent(cosas[11])
+                )
+                .addGroup(orden.createParallelGroup()
                     .addGroup(orden.createSequentialGroup()
-                        .addComponent(cosas[3], 150, 150, 150)
-                        .addComponent(cosas[4], 300, 300, 300)
+                        .addGroup(orden.createParallelGroup()
+                            .addComponent(cosas[3])
+                            .addComponent(cosas[9])
+                        )
+                    .addComponent(cosas[12])
                     )
                 )
         );
@@ -42,12 +55,28 @@ public class Ej2 extends JFrame{
         orden.setVerticalGroup(
             orden.createSequentialGroup()
                 .addGroup(orden.createParallelGroup()
-                        .addComponent(cosas[1])
+                    .addComponent(cosas[1])
+                    .addGroup(orden.createSequentialGroup()
                         .addComponent(cosas[2])
+                        .addComponent(cosas[5])
+                        .addGroup(orden.createParallelGroup()
+                            .addComponent(cosas[7])
+                            .addComponent(cosas[8])
+                        )
+                    )
+                    .addGroup(orden.createSequentialGroup()
+                        .addComponent(cosas[3])
+                        .addComponent(cosas[9])
+                    )
+                    .addGroup(orden.createSequentialGroup()
+                        .addComponent(cosas[4])
+                        .addComponent(cosas[6])
+                        .addComponent(cosas[10])
+                    )
                 )
                 .addGroup(orden.createParallelGroup()
-                    .addComponent(cosas[3])
-                    .addComponent(cosas[4])
+                    .addComponent(cosas[11])
+                    .addComponent(cosas[12])
                 )
         );
         
--- Sockets/src/Main.java
+++ Sockets/src/Main.java
@@ -1,4 +1,19 @@
-package PACKAGE_NAME;
+import java.io.IOException;
+import java.net.Socket;
 
 public class Main {
+    public static void main(String[] args) {
+        Socket soyCliente;
+        try {
+            soyCliente = new Socket("192.168.84.91", 5000);
+            String y = "";
+            for (int i = 0; i < 4; i++){
+                y += (char)soyCliente.getInputStream().read();
+            }
+            System.out.println(y);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
